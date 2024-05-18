e2e8c177e78b7c7ffeb8d3bb2e6b43135fd149b7
pawjamro <jamro.pawel@gmail.com>
2024-05-18 14:13:02 +0200

    Reaktywnosc

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
-    let email = 'fracz@agh.edu.pl';
+    //let email = 'pawel.jamro@student.agh.edu.pl';
+    const [email, setEmail] = useState('');
     function handleChange(event) {
-        console.log(event.target.value);}
+        setEmail(event.target.value);}
+    let message;
+    if (email.length < 10) {
+        message = <div>Ale masz krótki adres!</div>;
+    } else if (email.length < 15) {
+        message = <div>Twój adres e-mail jest w sam raz.</div>;
+    } else {
+        message = <div>Twój adres e-mail jest stanowczo za długi.</div>;
+    }
   return (
       <div>
           <h1>System do zapisów na zajęcia</h1>
           <h2>Twój e-mail to {email}</h2>
-          <input type="text" onChange={handleChange}/>
+          {message}
+          <input type="text" value={email} onChange={handleChange}/>
       </div>
   );
     }
